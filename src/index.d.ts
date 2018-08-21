0e977f883ff7cd5913608eafc40ee51871c9241f
Edouard Demotes-Mainard <e.demotes@gmail.com>
2018-08-21 17:12:44 -0400

    Add Typescript definition

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for Hapify Syntax
+
+/**
+ * Export the class
+ */
+export class HapifySyntax {
+    /**
+     * Run the code generation for this template's content and this explicit model
+     *
+     * @param {string} template
+     * @param {any} model
+     * @return {string}
+     */
+    static run(template: string, model: any): string;
+}
